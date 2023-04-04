76785f413f714a9a927de4a86a692bd8f43d5131
Alexis R. Santos-Lozada <appdemography@gmail.com>
2023-04-04 12:10:37 -0400

    Add files via upload

--- CODE/Falls_Replication_Code.R
+++ CODE/Falls_Replication_Code.R
@@ -40,7 +40,6 @@ model7<-(lm(log(White)~ID,data=data))
 summary(lm(log(White)~ID,data=data))
 confint(model7,level=0.95)
 
-
 summary(lm(log(White)~ID,data=data))
 
 library(ggplot2)
@@ -48,8 +47,10 @@ library(ggplot2)
 sex <- read_excel("/Data/Processed/Sex.xlsx")
 
 a<-ggplot(data=sex, aes(x=Year, y=Rate, color=Sex)) +
-  geom_line()+theme_bw()+
-  geom_point(size=1.5)+
+  geom_line(size=1)+theme_bw()+
+  geom_point(size=2.5)+
+  #geom_ribbon(aes(y=Rate, ymax=Upper, ymin=Lower,fill=Sex), 
+  #            alpha=0.35)+
   labs(title ="Death rates for falls among adults 65 years and older and by sex, 1999-2020",
        x = "", y= "AAMR per 100,000 persons")+
   scale_x_continuous(breaks = seq(from = 1998, to = 2020, by = 2))+
@@ -61,8 +62,10 @@ a
 race <- read_excel("C:/Users/ars39/Documents/Falls/R an R/Data/Processed/Race_Ethnicity.xlsx")
 
 b<-ggplot(data=race, aes(x=Year, y=AAMR, color=Race)) +
-  geom_line()+theme_bw()+
-  geom_point(size=1.5)+
+  geom_line(size=1)+theme_bw()+
+  geom_point(size=2.5)+
+  #geom_ribbon(aes(y=AAMR, ymax=AAMR_H, ymin=AAMR_L,fill=Race), 
+  #            alpha=0.05)+
   labs(title ="Death rates for falls among adults 65 and older by race/ethnicity, 1999-2020",
        x = "Year ", y= "AAMR per 100,000 persons")+
   scale_x_continuous(breaks = seq(from = 1998, to = 2020, by = 2))+
@@ -74,8 +77,7 @@ b
 library(ggpubr)
 ggarrange(a,b,labels=c("A","B"),ncol=1,common.legend = FALSE)
 
-#MULTI<-ggarrange(a,b,labels=c("A","B"),ncol=1,common.legend = FALSE)
+MULTI<-ggarrange(a,b,labels=c("A","B"),ncol=1,common.legend = FALSE)
 
-#library(rasterpdf)
+MULTI
 
-ggsave("Fig.pdf", width = 20, height = 20, units = "cm")
